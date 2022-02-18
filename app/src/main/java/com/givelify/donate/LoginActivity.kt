a3cec8160ccc23b6ec679d04349b8ba82836bbc0
kenamuigai <kenamuigai@gmail.com>
2022-02-18 14:18:19 +0300

    Connected Donate to Firebase Created Firebase Authentication for Email and Google Sign In RegistrationActivity.kt and LoginActivity.kt Create the UI for LoginActivity.kt and RegistrationActivity.kt in the activity_login.xml and activity_registration.xml Implemented View Binding Initialised firebase auth object in RegistrationActivity.kt and LoginActivity.kt Added a check password for RegistrationActivity.kt Added an on Click with intent for switching from RegistrationActivity.kt to LoginActivity.kt

--- app/src/main/java/com/givelify/donate/LoginActivity.kt
+++ app/src/main/java/com/givelify/donate/LoginActivity.kt
@@ -1,11 +1,99 @@
 package com.givelify.donate
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var tvRedirectSignUp: TextView
+    lateinit var etEmail: EditText
+    private lateinit var etPass: EditText
+    lateinit var btnLogin: Button
+
+    // Creating firebaseAuth object
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // View Binding
+        tvRedirectSignUp = findViewById(R.id.tvRedirectSignUp)
+        btnLogin = findViewById(R.id.btnLogin)
+        etEmail = findViewById(R.id.etEmailAddress)
+        etPass = findViewById(R.id.etPassword)
+
+        // initialising Firebase auth object
+        auth = FirebaseAuth.getInstance()
+
+        btnLogin.setOnClickListener {
+            login()
+        }
+
+        tvRedirectSignUp.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+            // using finish() to end the activity
+            finish()
+        }
+    }
+
+    private fun login() {
+        val email = etEmail.text.toString()
+        val pass = etPass.text.toString()
+        // calling signInWithEmailAndPassword(email, pass)
+        // function using Firebase auth object
+        // On successful response Display a Toast
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Successfully LoggedIn", Toast.LENGTH_SHORT).show()
+            } else
+                Toast.makeText(this, "Log In failed ", Toast.LENGTH_SHORT).show()
+        }
+        // Configure Google Sign In
+//        val gso = GoogleSignInOptions
+//            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//            .requestIdToken(getString(R.string.default_web_client_id))
+//            .requestEmail()
+//            .build()
+//
+//        var googleSignInClient = GoogleSignIn.getClient(this, gso)
+//    }
+
+//    @Override fun onStart() {
+//        super.onStart()
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        var currentUser = auth.getCurrentUser()
+//        updateUI(currentUser);
+//    }
+//    private fun firebaseAuthWithGoogle(idToken: String) {
+//        val credential = GoogleAuthProvider.getCredential(idToken, null)
+//        auth.signInWithCredential(credential)
+//            .addOnCompleteListener(this) { task ->
+//                if (task.isSuccessful) {
+//                    // Sign in success, update UI with the signed-in user's information
+//                    Log.d(TAG, "signInWithCredential:success")
+//                    val user = auth.currentUser
+//                    updateUI(user)
+//                } else {
+//                    // If sign in fails, display a message to the user.
+//                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+//                    updateUI(null)
+//                }
+//            }
+//        Firebase.auth.signOut()
+//    }
     }
 }
